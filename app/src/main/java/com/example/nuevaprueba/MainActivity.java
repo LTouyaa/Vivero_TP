45a81a5be0ab5f645b68ed384f59c02ea6ba3be0
lucas <argentina_93_manterola@hotmail.com>
2019-05-25 21:49:56 -0300

    Primeras modificaciones basicas y commit de prueba

--- app/src/main/java/com/example/nuevaprueba/MainActivity.java
+++ app/src/main/java/com/example/nuevaprueba/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.nuevaprueba;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nombre;
+    private EditText apellido;
+
+    private Button login;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        nombre = findViewById(R.id.edt_1);
+        apellido = findViewById(R.id.ed_2);
+        login = findViewById(R.id.btm_1);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Hola Mundo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
