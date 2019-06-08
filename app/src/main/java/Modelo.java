274fd272984df2b109cea138f778bd845e1a4880
pesch <peschiutta.lucho@gmail.com>
2019-06-08 19:09:32 -0300

    Primera implementacion tentativa de modelo

---
+++ app/src/main/java/Modelo.java
@@ -0,0 +1,36 @@
+import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class Modelo {
+
+    
+   private HashMap<Integer, Planta> vivero;
+
+
+
+   public Modelo(){
+       vivero = new HashMap<Integer, Planta>();
+   }
+
+   public void agregar(Integer lugar, Planta p){
+       if(vivero.containsKey(lugar)) {
+          // Toast.makeText(this, "Lugar ocupado", Toast.LENGTH_SHORT).show();
+       } else {
+           vivero.put(lugar, p);
+       }
+   }
+
+   public void remover(Integer lugar){
+       if(vivero.containsKey(lugar)){
+           vivero.remove(lugar);
+       } else {
+           // Toast.makeText(this, "El lugar estaba vacio", Toast.LENGTH_SHORT).show();
+       }
+   }
+
+   public Collection<Planta> obtenerColeccion(){
+       return vivero.values();
+   }
+}
