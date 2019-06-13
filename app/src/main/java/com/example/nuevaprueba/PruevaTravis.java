6424a5409dbd63b201d51bb7e6b01c068e340268
pesch <peschiutta.lucho@gmail.com>
2019-06-13 10:30:35 -0300

    Para probar integracion con travis

---
+++ app/src/main/java/com/example/nuevaprueba/PruevaTravis.java
@@ -0,0 +1,13 @@
+package com.example.nuevaprueba;
+
+public class PruevaTravis {
+    private int numero;
+
+    public PruevaTravis(int numero){
+        this.numero=numero;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+}
