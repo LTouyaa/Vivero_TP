d889347971ebe524a6b8bafef4e1d74bb3123dbe
pesch <peschiutta.lucho@gmail.com>
2019-06-25 16:37:44 -0300

    Primeros pasos fragment agregar_planta

---
+++ app/src/main/java/com/TP/Vivero/Vista/Fragment/AgregarPlantaFragment.java
@@ -0,0 +1,28 @@
+package com.TP.Vivero.Vista.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.TP.Vivero.R;
+
+public class AgregarPlantaFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_agregar_planta,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        public
+
+
+    }
+}
