ee822738cf285fd90b1e7a8c930f1d36169855cb
Maria Belen Tracchia <tracchiabelen@gmail.com>
2019-06-25 20:31:08 -0300

    Agregue una clase que da temperaturas y un porcentaje alealorio. La temperatura es de -10 a 50°. El porcentaje de 0 a 100, serviria para humedad y luz.

---
+++ app/src/main/java/com/TP/Vivero/Model/SimuladorAleatorios.java
@@ -0,0 +1,32 @@
+package com.TP.Vivero.Model;
+
+import java.util.Random;
+
+public class SimuladorAleatorios {
+
+    Random random;
+
+    public SimuladorAleatorios()
+    {
+        random= new Random();
+    }
+
+
+
+    public int  getTempSimulada()
+  {
+      int temp;
+
+      temp = random.nextInt(60)-10;
+
+      return temp;
+  }
+
+  public int getPorcentajeSimulado()
+  {
+      int hum =random.nextInt(100);
+        return hum;
+  }
+
+
+}
--- app/src/main/java/com/TP/Vivero/Vista/Fragment/AgregarFragment.java
+++ app/src/main/java/com/TP/Vivero/Vista/Fragment/AgregarFragment.java
@@ -1,20 +1,42 @@
 package com.TP.Vivero.Vista.Fragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.TP.Vivero.Model.SimuladorAleatorios;
 import com.TP.Vivero.R;
 
 public class AgregarFragment extends Fragment {
+
+
+    Context context;
+
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_agregar, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getActivity();
+
+
+
+
+
+
+        }
 }
